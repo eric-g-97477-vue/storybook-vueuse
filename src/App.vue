b939ab80ff707e19c29a9db1b15e47746abbd06c
Eric Gorr <egorr@px.ftw>
2024-12-12 17:24:51 -0500

    repro

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+	// import HelloWorld from './components/HelloWorld.vue';
+	// import TheWelcome from './components/TheWelcome.vue';
+	import Button from './stories/Button.vue';
+
+	import { useDark, useToggle } from '@vueuse/core';
+
+	const isDark = useDark();
+
+	function togglemode() {
+		isDark.value = !isDark.value;
+		console.log('Toggle Mode', isDark.value);
+	}
+</script>
+
+<template>
+	<button @click="togglemode">Toggle</button>
+	<Button label="Report Mode" :primary="true"></Button>
+</template>
+
+<style scoped></style>
